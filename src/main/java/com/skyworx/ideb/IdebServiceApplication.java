72452de4e2a0f4573fb061564f8d0cbc59139b5c
ardiwiraansori <120303612+ardiwiraansori@users.noreply.github.com>
2026-09-14 20:29:17 +0700

    Initial Spring Boot project setup

---
+++ src/main/java/com/skyworx/ideb/IdebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.skyworx.ideb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skyworx/ideb/IdebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyworx.ideb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
